a3bb39db820bd79c039cc702d4c30bde33adb6bf
John Vrbanac <john.vrbanac@rackspace.com>
2014-12-19 13:49:13 -0600

    Fixing install kickstart and vmware tools install

--- scripts/redhat_vmtools.sh
+++ scripts/redhat_vmtools.sh
@@ -17,16 +17,35 @@ fi
 
 # VMWare Tools
 if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
+    ISO_MOUNT="/media"
+    VMWARE_INSTALL_SCRIPT="/tmp/vmware/vmware-tools-distrib/vmware-install.pl"
+    VMWARE_TOOLS_ISO="/home/vagrant/linux.iso"
+    VMWARE_TOOLS_TMP="/tmp/vmware"
+
     # Install pre-reqs
     yum -y install fuse fuse-libs
 
-    # Mount and Install
-    mount -o loop /home/vagrant/linux.iso /mnt
-    tar zxf /mnt/VMwareTools-*.tar.gz
-    /tmp/vmware-tools-distrib/vmware-install.pl --default
+    echo "Mounting ${VMWARE_TOOLS_ISO} onto ${ISO_MOUNT}"
+    mount -o loop ${VMWARE_TOOLS_ISO} ${ISO_MOUNT}
+
+    echo "Extracting VMWare Tools to /tmp/vmware"
+    mkdir -p ${VMWARE_TOOLS_TMP}
+
+    echo "Extracting ${ISO_MOUNT}/VMwareTools-*.tar.gz to ${VMWARE_TOOLS_TMP}"
+    tar xzf ${ISO_MOUNT}/VMwareTools-*.tar.gz -C ${VMWARE_TOOLS_TMP}
+
+    echo "Unmounting ${VMWARE_TOOLS_ISO} from ${ISO_MOUNT}"
+    umount ${ISO_MOUNT}
+
+    echo "Running ${VMWARE_INSTALL_SCRIPT}"
+    ${VMWARE_INSTALL_SCRIPT} -d
+
+    # Enable auto build
+    # sed -i "s/AUTO_KMODS_ENABLED_ANSWER no/AUTO_KMODS_ENABLED_ANSWER yes/g" /etc/vmware-tools/locations
+    # sed -i "s/AUTO_KMODS_ENABLED no/AUTO_KMODS_ENABLED yes/g" /etc/vmware-tools/locations
 
     # Clean up
-    umount /mnt
-    rm -rf /tmp/vmware-tools-distrib
-    rm -rf /home/vagrant/linux.iso
+    echo "Cleaning up ${VMWARE_TOOLS_ISO} and ${VMWARE_TOOLS_TMP}"
+    rm ${VMWARE_TOOLS_ISO}
+    rm -rf ${VMWARE_TOOLS_TMP}
 fi
